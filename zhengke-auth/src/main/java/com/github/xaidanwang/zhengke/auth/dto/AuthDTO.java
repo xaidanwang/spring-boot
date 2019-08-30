61f13b5d7c2446768e7ab352fb0f375354006d38
xaidanwang <wangx982770631@163.com>
2019-08-30 16:38:49 +0800

    spring boot 脚手架完善 0.3 版本

---
+++ zhengke-auth/src/main/java/com/github/xaidanwang/zhengke/auth/dto/AuthDTO.java
@@ -0,0 +1,13 @@
+package com.github.xaidanwang.zhengke.auth.dto;
+
+/**
+ * @author wang yi fei
+ * @date 2019/8/29 20:30
+ */
+public class AuthDTO {
+	private String id;
+
+	private String token;
+
+	private String expire;
+}
