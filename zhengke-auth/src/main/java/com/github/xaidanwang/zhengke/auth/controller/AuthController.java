7895e7b71370be04fb44b2827644aadbbf3f5fee
xaidanwang <wangx982770631@163.com>
2019-09-16 13:31:22 +0800

    修改查询bug where 改成使用 <where>

--- zhengke-auth/src/main/java/com/github/xaidanwang/zhengke/auth/controller/AuthController.java
+++ zhengke-auth/src/main/java/com/github/xaidanwang/zhengke/auth/controller/AuthController.java
@@ -27,7 +27,7 @@ public class AuthController {
 	@Autowired
 	private AuthService authService;
 
-	@RequestMapping(value = "/expireTime",method = RequestMethod.GET)
+	@RequestMapping(value = "/expireTime/list",method = RequestMethod.GET)
 	public CommonResult<List<AuthDo>> getExpireTime(@RequestParam(value = "phoneid",required = false) String phoneid,@RequestParam(value = "token",required = false) String token,@RequestParam(value = "remark",required = false)String remark){
 
 		if (StringUtils.isEmpty(phoneid) && StringUtils.isEmpty(token)){
@@ -38,44 +38,44 @@ public class AuthController {
 		return CommonResult.buildWithData(ConstantEnum.GLOBAL_SUCCESS,result);
 	}
 
-	@ApiOperation(value = "给某个秘钥或者某个备注下的秘钥增加过期时间",httpMethod = "POST",notes = "给某个秘钥或者某个备注下的秘钥增加过期时间")
-	@RequestMapping(value = "/expireTime",method = RequestMethod.POST)
+	@ApiOperation(value = "给某个秘钥或者某个备注下的秘钥增加过期时间",httpMethod = "GET",notes = "给某个秘钥或者某个备注下的秘钥增加过期时间")
+	@RequestMapping(value = "/expireTime",method = RequestMethod.GET)
 	public CommonResult setTokenExpireTime(@RequestParam(value = "token",required = false) String token,@RequestParam(value = "remark",required = false)String remark, @RequestParam(value = "time")Long time){
 		authService.setTokenExpireTime(token,time,remark);
 		return CommonResult.build(ConstantEnum.GLOBAL_SUCCESS);
 	}
 
 
-	@ApiOperation(value = "批量生成token",httpMethod = "POST",notes = "批量生成token")
-	@PostMapping(value = "/add")
+	@ApiOperation(value = "批量生成token",httpMethod = "GET",notes = "批量生成token")
+	@GetMapping(value = "/add")
 	public CommonResult addToken(@RequestParam(value = "mum")Integer num, @RequestParam(value = "remark")String remark, @RequestParam(value = "time")Integer time){
 		authService.addToken(num,remark,time);
 		return CommonResult.build(ConstantEnum.GLOBAL_SUCCESS);
 	}
 
-	@PostMapping(value = "/bind")
-	@ApiOperation(value = "机器绑定秘钥",httpMethod = "POST",notes = "机器绑定秘钥")
+	@GetMapping(value = "/bind")
+	@ApiOperation(value = "机器绑定秘钥",httpMethod = "GET",notes = "机器绑定秘钥")
 	public CommonResult bindToken(@RequestParam(value = "phoneid")String phoneid, @RequestParam(value = "token")String token){
 		authService.bindToken(phoneid,token);
 		return CommonResult.build(ConstantEnum.GLOBAL_SUCCESS);
 	}
 
-	@ApiOperation(value = "批量或者单个查询",httpMethod = "POST",notes = "批量或者单个查询")
-	@PostMapping(value = "/get")
+	@ApiOperation(value = "批量或者单个查询",httpMethod = "GET",notes = "批量或者单个查询")
+	@GetMapping(value = "/get")
 	public CommonResult<List<AuthDo>> getAccountList(@RequestParam(value = "phoneid",required = false)String phoneid, @RequestParam(value = "token",required = false)String token, @RequestParam(value = "remark",required = false)String remark){
 		List<AuthDo> authDoList = authService.getAccountList(phoneid,token,remark);
 		return CommonResult.buildWithData(ConstantEnum.GLOBAL_SUCCESS,authDoList);
 	}
 
-	@ApiOperation(value = "删除所有过期的秘钥",httpMethod = "DELETE",notes = "删除所有过期的秘钥")
-	@RequestMapping(value = "/token",method = RequestMethod.DELETE)
+	@ApiOperation(value = "删除所有过期的秘钥",httpMethod = "GET",notes = "删除所有过期的秘钥")
+	@RequestMapping(value = "/token",method = RequestMethod.GET)
 	public CommonResult<String> clearExpireToken(){
 		String result = authService.clearExpireToken();
 		return CommonResult.buildWithData(ConstantEnum.GLOBAL_SUCCESS,result);
 	}
 
-	@ApiOperation(value = "账号秘钥检测",httpMethod = "POST",notes = "账号秘钥检测")
-	@RequestMapping(value = "/verify",method = RequestMethod.POST)
+	@ApiOperation(value = "账号秘钥检测",httpMethod = "GET",notes = "账号秘钥检测")
+	@RequestMapping(value = "/verify",method = RequestMethod.GET)
 	public CommonResult atuhVerify(@RequestParam(value = "phoneid") String phoneid,@RequestParam(value = "token") String token){
 		if (StringUtils.isEmpty(token) || token.length() != 32){
 			throw new ParamException("秘钥不能为空或者格式不对");
